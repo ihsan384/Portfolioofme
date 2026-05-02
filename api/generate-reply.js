b8367eec44dfaec94d9a234e44e18ee6b26e6786
ihsan <ihsan.anas8281@gmail.com>
2026-05-02 10:39:43 +0530

    Updated logic

--- api/generate-reply.js
+++ api/generate-reply.js
@@ -14,8 +14,8 @@ export default async function handler(req, res) {
       return res.status(400).json({ error: "Message required" });
     }
 
-    const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
+   const response = await fetch(
+  `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
       {
         method: "POST",
         headers: {
